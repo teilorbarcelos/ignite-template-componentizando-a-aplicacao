6f8eba56c592c01d1f0a3cbbb7b5b4990bb20f30
Teilor Souza Barcelos <tsb-@live.com>
2021-12-03 10:33:06 -0300

    componentization finished :tada:

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+type Props = {
+  handleClickButton: (id: number) => void
+  selectedGenreId: number
+  genres: GenreResponseProps[]
+}
+
+export function SideBar({ handleClickButton, genres, selectedGenreId }: Props) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
